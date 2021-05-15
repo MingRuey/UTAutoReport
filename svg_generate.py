1e29509dbf81c7899963880c608fabc8a2b90add
mrchou <imchou239@gmail.com>
2021-05-16 02:17:46 +0800

    Attempt on converting texts into svg

--- svg_generate.py
+++ svg_generate.py
@@ -1,2 +1,15 @@
+import svgwrite
+
+
 if __name__ == "__main__":
-    pass
+    dwg = svgwrite.Drawing(
+        filename="/home/mrchou/code/AxisDraw/sample/draw.svg",
+        height="29.7cm", width="21cm"
+    )
+    dwg.add(
+        dwg.text(
+            "This is some text",
+            insert=("1cm", "1cm")
+        )
+    )
+    dwg.save()
